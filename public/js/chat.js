1dbce78598517ac09ad5b3375d2bae1ccf19f538
leeandri <rl.tsirih@gmail.com>
2025-04-05 00:14:52 +0300

    Init commit

---
+++ public/js/chat.js
@@ -0,0 +1,146 @@
+const socket = io();
+
+// Elements
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $sendLocationButton = document.querySelector("#send-location");
+const $messages = document.querySelector("#messages");
+
+// Templates
+const messageTemplate = document.querySelector("#message-template").innerHTML;
+const urlTemplate = document.querySelector("#url-template").innerHTML;
+const sidebarTemplate = document.querySelector("#sidebar-template").innerHTML;
+
+// Options
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
+
+let lastActiveUser = null;
+
+const autoscroll = () => {
+  // New message element
+  const $newMessage = $messages.lastElementChild;
+
+  // Height of new  message
+  const newMessageStyles = getComputedStyle($newMessage);
+  const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+  const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+
+  // Visible height
+  const visibleHeight = $messages.offsetHeight;
+
+  // Height of messages container
+  const containerHeight = $messages.scrollHeight;
+
+  // How far have I scrolled?
+  const scrollOffset = $messages.scrollTop + visibleHeight;
+
+  if (containerHeight - newMessageHeight <= scrollOffset) {
+    $messages.scrollTop = $messages.scrollHeight;
+  }
+};
+
+socket.on("message", (message) => {
+  console.log(message);
+  const html = Mustache.render(messageTemplate, {
+    username: message.username,
+    message: message.text,
+    createdAt: moment(message.createdAt).format("h:mm a"),
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+  autoscroll();
+
+  if (message.username !== "Admin") {
+    lastActiveUser = message.username; // Update active user
+  }
+  updateUserStyles();
+});
+
+// reapply styles
+function updateUserStyles() {
+  const userElements = document.querySelectorAll(".users li");
+  //Reapply "active-user" only if the user exists in the list
+  userElements.forEach((userElement) => {
+    if (userElement.textContent === lastActiveUser) {
+      userElement.classList.add("active-user");
+    } else {
+      userElement.classList.remove("active-user");
+    }
+  });
+}
+
+socket.on("locationMessage", (location) => {
+  console.log(location);
+  const html = Mustache.render(urlTemplate, {
+    username: location.username,
+    url: location.url,
+    createdAt: moment(location.createdAt).format("h:mm a"),
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+  autoscroll();
+
+  // Activate user too here when he/she/they send location :)
+  lastActiveUser = location.username; // save the user who shared their location
+  updateUserStyles();
+});
+
+socket.on("roomData", ({ room, users }) => {
+  const html = Mustache.render(sidebarTemplate, {
+    room,
+    users,
+  });
+  document.querySelector("#sidebar").innerHTML = html;
+
+  updateUserStyles();
+});
+
+$messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  $messageFormButton.setAttribute("disabled", "disabled");
+
+  const message = e.target.elements.message.value;
+
+  socket.emit("sendMessage", message, (error) => {
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+
+    if (error) {
+      return console.log(error);
+    }
+
+    console.log("Message delivered!");
+  });
+});
+
+$sendLocationButton.addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported by your browser");
+  }
+
+  $sendLocationButton.setAttribute("disabled", "disabled");
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit(
+      "sendLocation",
+      {
+        longitude: position.coords.longitude,
+        latitude: position.coords.latitude,
+      },
+      () => {
+        console.log("Location shared!");
+        $sendLocationButton.removeAttribute("disabled");
+      }
+    );
+  });
+});
+
+socket.emit("join", { username, room }, (error) => {
+  if (error) {
+    alert(error);
+    location.href = "/";
+  }
+});
